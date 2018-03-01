c651e3ae1c363620f4a09a07e2c402c3e98c5aba
sharthak ghosh <sharthak.ghosh@live.in>
2018-03-02 00:44:17 +0530

    Remove old

--- StellerCore/IDockerClientWrapper.cs
+++
@@ -1,9 +0,0 @@
-using Docker.DotNet;
-
-namespace StellerAPI.StellerCore
-{
-    public interface IDockerClientWrapper
-    {
-        DockerClient GetDockerClient(string uri = "npipe://./pipe/docker_engine");
-    }
-}
